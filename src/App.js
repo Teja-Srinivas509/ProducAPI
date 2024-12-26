dea05d01d207790967c2702bb42f8d0ce6ee925a
Teja-Srinivas509 <cheedallatejasrinivas@gmail.com>
2024-12-26 20:40:31 +0530

    Added initial code for the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductCRUD from './ProductCRUD';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductCRUD />
+    </div>
+  );
+}
+
+export default App;
